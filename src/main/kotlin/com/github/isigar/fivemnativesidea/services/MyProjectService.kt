dc93c3a0fd2b84acccbfd6b1cb765ccabb5192b0
GitHub Action <action@github.com>
2021-02-25 19:17:29 +0000

    Template cleanup

--- src/main/kotlin/com/github/isigar/fivemnativesidea/services/MyProjectService.kt
+++ src/main/kotlin/com/github/isigar/fivemnativesidea/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.isigar.fivemnativesidea.services
 
+import com.github.isigar.fivemnativesidea.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
